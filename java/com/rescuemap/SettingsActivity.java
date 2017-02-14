5b62c119cdfca339965241b2929517fc9eff864f
Şamil Korkmaz <samil.korkmaz@gmail.com>
2017-02-14 20:07:40 +0300

    Get version

--- java/com/rescuemap/SettingsActivity.java
+++ java/com/rescuemap/SettingsActivity.java
@@ -1,15 +1,19 @@
 package com.rescuemap;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class SettingsActivity extends AppCompatActivity {
 
     EditText etChild7YearsOldSpeed_mps;
     EditText etChild15YearsOldSpeed_mps;
     EditText etAdult30YearsOldSpeed_mps;
+    TextView tvAbout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class SettingsActivity extends AppCompatActivity {
         etChild7YearsOldSpeed_mps = (EditText) findViewById(R.id.etChild7YearsOldSpeed_mps);
         etChild15YearsOldSpeed_mps = (EditText) findViewById(R.id.etChild15YearsOldSpeed_mps);
         etAdult30YearsOldSpeed_mps = (EditText) findViewById(R.id.etAdult30YearsOldSpeed_mps);
+        tvAbout = (TextView) findViewById(R.id.tvAbout);
         showSettings();
     }
 
@@ -25,6 +30,7 @@ public class SettingsActivity extends AppCompatActivity {
         etChild7YearsOldSpeed_mps.setText(String.valueOf(MySettings.getChild7YearsOldSpeedMps()));
         etChild15YearsOldSpeed_mps.setText(String.valueOf(MySettings.getChild15YearsOldSpeedMps()));
         etAdult30YearsOldSpeed_mps.setText(String.valueOf(MySettings.getAdult30YearsOldSpeedMps()));
+        tvAbout.setText("Version " + getVersionName() + ", copyright Şamil Korkmaz");
     }
 
     public void btnSaveClick(View view) {
@@ -42,5 +48,15 @@ public class SettingsActivity extends AppCompatActivity {
         etChild7YearsOldSpeed_mps.setText(String.valueOf(MySettings.getChild7YearsOldDefaultSpeedMps()));
         etChild15YearsOldSpeed_mps.setText(String.valueOf(MySettings.getChild15YearsOldDefaultSpeedMps()));
         etAdult30YearsOldSpeed_mps.setText(String.valueOf(MySettings.getAdult30YearsOldDefaultSpeedMps()));
+    }
+
+    public String getVersionName() {
+        PackageInfo pInfo = null;
+        try {
+            pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return pInfo.versionName;
     }
 }
